ba15e80d374d2f4d0a56016fd710ed97cda264c6
Michael Thomsen <nesmomik@gmail.com>
2025-11-16 08:11:02 +0100

    Added missing comments

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,9 +1,11 @@
 import json
 
 
+# file paths
 HTML_TEMPLATE_FILE = "animals_template.html"
 JSON_DATA_FILE = "animals_data.json"
 HTML_OUTPUT_FILE = "animals.html"
+
 
 def read_template(file_path):
     """Returns the content of the template file as string"""
@@ -12,6 +14,7 @@ def read_template(file_path):
 
 
 def create_html_string():
+    """reads the template file and replaces the placeholder text with the generated html code"""
     return read_template(HTML_TEMPLATE_FILE).replace(
         "__REPLACE_ANIMALS_INFO__", format_data()
     )
@@ -33,7 +36,7 @@ def format_data():
     """Aggregates and returns a string from the data"""
     animals_data = load_data(JSON_DATA_FILE)
 
-    data_string = ''
+    data_string = ""
 
     for animal in animals_data:
         data_string += serialize_animal(animal)
@@ -42,15 +45,17 @@ def format_data():
 
 
 def serialize_animal(animal):
-    data_string = ''
+    """wraps data retrieved from a single object in html code"""
+    data_string = ""
+    # add html and data to output string
     data_string += '<li class="cards__item">'
-    data_string += f'<div class="card__title">{animal.get('name')}</div>\n'
+    data_string += f'<div class="card__title">{animal.get("name")}</div>\n'
     data_string += '<div class="card__text">\n<ul>\n'
     data_string += f"<li><strong>Scientific Name:</strong> {animal.get('taxonomy').get('scientific_name')}</li>\n"
     if animal.get("characteristics").get("diet") is not None:
         data_string += f"<li><strong>Diet:</strong> {animal.get('characteristics').get('diet')}</li>\n"
     data_string += f"<li><strong>Location(s):</strong> {animal.get('locations')[0]}"
-    for i in range(1, len(animal.get('locations'))):
+    for i in range(1, len(animal.get("locations"))):
         data_string += f", {animal.get('locations')[i]}"
     data_string += "</li>\n"
     if animal.get("characteristics").get("weight") is not None:
@@ -61,13 +66,15 @@ def serialize_animal(animal):
         data_string += f"<li><strong>Temperament:</strong> {animal.get('characteristics').get('temperament')}</li>\n"
     if animal.get("characteristics").get("slogan") is not None:
         data_string += f"<li><strong>Slogan:</strong> {animal.get('characteristics').get('slogan')}</li>\n"
-    data_string += '</ul></div></li>'
-    
+    data_string += "</ul></div></li>"
+
     return data_string
 
 
 def main():
+    """Reads a template html file, adds html derived form json data and writes the result to new file."""
     write_html(HTML_OUTPUT_FILE, create_html_string())
+
 
 if __name__ == "__main__":
     main()
